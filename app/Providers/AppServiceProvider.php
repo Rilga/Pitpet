5bc20a42dfc2ecbe363dc799a8080ad1df8de992
rilgalingga <rilgalingga5@gmail.com>
2025-12-09 23:44:56 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production', 'staging')) {
         // Memaksa Laravel untuk menghasilkan URL aset menggunakan HTTPS
         URL::forceScheme('https'); 
-    }
+        }
+        if (env('VERCEL_ENV') === 'production' && env('APP_DEBUG') !== 'true') {
+        // Abaikan variabel lingkungan dan set debug ke true
+        config(['app.debug' => true]);
+        }
     }
 }
