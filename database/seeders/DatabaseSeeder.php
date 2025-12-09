1674064bf2717a62508f66e0013c876db0d59452
rilgalingga <rilgalingga5@gmail.com>
2025-12-09 20:54:51 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,16 +2,43 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call([
-            PetServicesSeeder::class,
+        // 1. Akun Admin
+        User::create([
+            'name' => 'Admin Pitpet',
+            'email' => 'admin@pitpet.com', // Ganti dengan email yang Anda inginkan
+            'password' => Hash::make('admin12345'), // Ganti 'password' dengan password yang aman
+            'role' => 'admin',
         ]);
+
+        // 2. Akun Groomer (User)
+        User::create([
+            'name' => 'Groomer Satu',
+            'email' => 'groomer1@pitpet.com', 
+            'password' => Hash::make('user12345'),
+            'role' => 'user',
+        ]);
+
+        User::create([
+            'name' => 'Groomer Dua',
+            'email' => 'groomer2@pitpet.test', 
+            'password' => Hash::make('user12345'), 
+            'role' => 'user',
+        ]);
+
+        User::create([
+            'name' => 'Groomer Tiga',
+            'email' => 'groomer3@pitpet.test', 
+            'password' => Hash::make('user12345'), 
+            'role' => 'user',
+        ]);
+        
     }
 }
